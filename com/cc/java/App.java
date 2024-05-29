641c371161331af1ec1407a7345894a83e8db80a
Jonsy86 <jonasnolte86@gmail.com>
2024-05-29 13:02:07 +0200

    interfaces

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,11 +1,13 @@
 package com.cc.java;
 
+import java.text.Bidi;
+
 public class App {
     
     public static void main(String[] args) {
 
-      
-     
+      Bird bird = new Bird();
+  
     }
 
     private static void output(String outputStr) {
